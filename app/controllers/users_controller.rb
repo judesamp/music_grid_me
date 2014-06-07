90e30d237c230c1edcef687b27a9f0085eb4d7c5
Jeremy Samples <judesamp@gmail.com>
2014-06-07 13:23:09 -0400

    created taste profiles controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,16 @@ class UsersController < ApplicationController
   def dashboard
     @graph = Koala::Facebook::API.new(current_user.facebook_token)
     @music_likes = @graph.get_object("me/music", {}, api_version: "v2.0")
-    x = Echowrap.taste_profile_create(:name => 'My new playlist3', :type => 'song')
-    puts x.id
+    data = []
+    puts @music_likes
+    @music_likes.each do |artist|
+      puts artist['name']
+      artist_hash = {"item" => {"artist_name" => "#{artist['name']}"}}
+      puts artist_hash
+      data << artist_hash
+    end
+    x = Echowrap.taste_profile_create(:name => 'oo', :type => 'artist')
+    y = Echowrap.taste_profile_update(:id => "#{x.id}", :data => data.to_json)
   end
 
   private
