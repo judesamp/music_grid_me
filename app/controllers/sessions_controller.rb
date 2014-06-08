346ebe25f1e0475f3df87db3ca479f0a08fd13fa
Jeremy Samples <judesamp@gmail.com>
2014-06-08 13:53:07 -0400

    testing ssl stuff with facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,6 +28,7 @@ class SessionsController < ApplicationController
 
 
   def omniauth_create
+    puts 'we are here!!!!!'
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     set_user_token(auth, user)
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,15 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.facebook_id, Rails.application.secrets.facebook_secret, :scope => 'email,user_birthday,read_stream,publish_actions,user_likes'
+  provider :facebook, Rails.application.secrets.facebook_id, Rails.application.secrets.facebook_secret, :scope => 'email,user_birthday,read_stream,publish_actions,user_likes', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
 end
+
+
+# OmniAuth.config.logger = Rails.logger
+
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   if Rails.env.development?
+#     OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+#     provider :facebook, 'DEV_APP_ID', 'DEV_APP_SEVRET'
+#   else
+#     provider :facebook, 'DEPLOY_APP_ID', 'DEPLOY_APP_SECRET'
+#   end
+# end
